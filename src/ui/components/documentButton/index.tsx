1340745176fa0c1c2202a55c3fe07c6e70d94e2a
mbaluev <mikhail.baluev@gmail.com>
2023-05-29 19:17:59 +0800

    refactor

--- src/ui/components/documentButton/index.tsx
+++ src/ui/components/documentButton/index.tsx
@@ -5,6 +5,7 @@ import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import { IDocumentDTO } from '@model/common/document';
+import { FormField } from '@components/form';
 
 interface IProps {
   doc?: IDocumentDTO;
@@ -38,34 +39,52 @@ export const DocumentButton = observer((props: IProps) => {
     size: 'medium',
     variant: 'outlined',
   };
+  const titleOpenLink = 'Open link in new tab';
+  const titleDownload = 'download file';
 
   if (documentType === 'link') {
     return (
-      <Button {...buttonProps} onClick={handleOpenLink} startIcon={iconLink}>
-        {doc.url}
-      </Button>
+      <FormField title={titleOpenLink}>
+        <Button {...buttonProps} onClick={handleOpenLink} startIcon={iconLink}>
+          {doc.url}
+        </Button>
+      </FormField>
     );
   }
   if (documentType === 'video') {
     if (doc.file) {
       return (
-        <Button {...buttonProps} onClick={handleDownload} startIcon={iconFile}>
-          {doc.file.name}
-        </Button>
+        <FormField title={titleDownload}>
+          <Button
+            {...buttonProps}
+            onClick={handleDownload}
+            startIcon={iconFile}
+          >
+            {doc.file.name}
+          </Button>
+        </FormField>
       );
     } else {
       return (
-        <Button {...buttonProps} onClick={handleOpenLink} startIcon={iconVideo}>
-          {doc.url}
-        </Button>
+        <FormField title={titleOpenLink}>
+          <Button
+            {...buttonProps}
+            onClick={handleOpenLink}
+            startIcon={iconVideo}
+          >
+            {doc.url}
+          </Button>
+        </FormField>
       );
     }
   }
   if (documentType === 'file' && doc.file) {
     return (
-      <Button {...buttonProps} onClick={handleDownload} startIcon={iconFile}>
-        {doc.file.name}
-      </Button>
+      <FormField title={titleDownload}>
+        <Button {...buttonProps} onClick={handleDownload} startIcon={iconFile}>
+          {doc.file.name}
+        </Button>
+      </FormField>
     );
   }
 
