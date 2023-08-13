db25d36b69f0f107235f713b920f0b24e6f3081b
mbaluev <mikhail.baluev@gmail.com>
2023-08-13 21:46:50 +0300

    admin question comment

--- src/core/components/breadCrumbs/index.tsx
+++ src/core/components/breadCrumbs/index.tsx
@@ -4,7 +4,6 @@ import { BreadCrumb, TBreadCrumb } from '@components/breadCrumbs/breadCrumb';
 import { IconButton } from '@components/iconButton';
 import { useRouter } from 'next/router';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './index.scss';
 
 interface IProps {
@@ -16,17 +15,11 @@ export const BreadCrumbs: FC<IProps> = (props) => {
   const { className, breadCrumbs } = props;
   const router = useRouter();
   const handleBack = () => router.back();
-  const handleForward = () => {
-    if (typeof window !== 'undefined') window.history.forward();
-  };
   const cls = classNames('bread-crumbs', className);
   return (
     <div className={cls}>
       <IconButton color="grey" size="small" onClick={handleBack}>
         <ArrowBackIcon />
-      </IconButton>
-      <IconButton color="grey" size="small" onClick={handleForward}>
-        <ArrowForwardIcon />
       </IconButton>
       {breadCrumbs?.map((item, index, array) => (
         <BreadCrumb key={index} {...item} last={index === array.length - 1} />
