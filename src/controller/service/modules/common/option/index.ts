1a29dce7cc78b1b696cd64d29a3ac48a02cc54b9
mbaluev <mikhail.baluev@gmail.com>
2023-09-03 22:54:05 +0300

    getDocumentTypes

--- src/controller/service/modules/common/option/index.ts
+++ src/controller/service/modules/common/option/index.ts
@@ -14,7 +14,7 @@ export class OptionService implements IOptionService {
   getDocumentTypes = async (
     token?: string | null
   ): Promise<ISelectItem[] | undefined> => {
-    const ret = await this.apiModule.post<IResponseListDTO<ISelectItem>>(
+    const ret = await this.apiModule.get<IResponseListDTO<ISelectItem>>(
       `${this.API_PREFIX}/list`,
       undefined,
       { headers: { Authorization: `Bearer ${token}` } }
