5fb69f6c769e492b445aada529139c58625455d2
mbaluev <mikhail.baluev@gmail.com>
2023-12-10 22:29:23 +0100

    local-auth-latest

--- src/ui/layout/layout/index.tsx
+++ src/ui/layout/layout/index.tsx
@@ -1,22 +1,16 @@
 import { ReactNode } from 'react';
 import { observer } from 'mobx-react';
-import { useAuthStore } from '@store/modules/common/auth/useAuthStore';
-import { DialogSessionExpired } from '@ui/dialogs/dialogSessionExpired';
 import { Navigation } from '@ui/layout/navigation';
 import Stack from '@mui/material/Stack';
 
 export const Layout = observer((props: { children?: ReactNode }) => {
   const { children } = props;
-  const { currentAccount, signOut, signIn } = useAuthStore();
+  // const { isAuth } = useAuthStore();
   return (
     <Stack height="100%" id="__layout">
       <Navigation />
       {children}
-      <DialogSessionExpired
-        open={currentAccount?.expired}
-        onLogin={signIn}
-        onLogout={signOut}
-      />
+      {/*<DialogSessionExpired open={!isAuth} />*/}
     </Stack>
   );
 });
--- src/ui/layout/navigation/index.tsx
+++ src/ui/layout/navigation/index.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Menu } from '@ui/layout/navigation/menu';
 import { Notifier } from '@ui/layout/notifier';
-import { Bar } from '@ui/layout/navigation/bar';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import { useElementSize } from 'usehooks-ts';
@@ -22,7 +21,6 @@ export const Navigation = () => {
       <AppBar id="__header" elevation={0} ref={ref}>
         <Menu />
         <Notifier />
-        <Bar />
       </AppBar>
       <Stack sx={{ visibility: 'hidden' }}>
         <Toolbar />
