27edfca7e2ab937a099b674789ada5a336220efd
mbaluev <mikhail.baluev@gmail.com>
2023-12-22 11:34:23 +0100

    core

--- src/ui/layout/page/breadCrumbs.tsx
+++ src/ui/layout/page/breadCrumbs.tsx
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 import Stack from '@mui/material/Stack';
 import { Button, IconButton } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { Url } from 'next/dist/shared/lib/router/router';
 import { Panel } from '@ui/layout/page/panel';
@@ -19,20 +18,15 @@ interface IProps {
 
 export const BreadCrumbs = (props: IProps) => {
   const { breadCrumbs } = props;
-  const router = useRouter();
-  const handleBack = async () => {
-    if (breadCrumbs && breadCrumbs.length > 1) {
-      const breadCrumb = breadCrumbs[breadCrumbs.length - 2];
-      if (breadCrumb.url) await router.push(breadCrumb.url);
-    }
-  };
   return (
     <Panel sx={{ p: 1 }}>
       <Stack direction="row" spacing={1} alignItems="center" flexWrap="wrap">
         {breadCrumbs && breadCrumbs.length > 1 && (
-          <IconButton size="small" color="secondary" onClick={handleBack}>
-            <ArrowBackIcon />
-          </IconButton>
+          <Link href={breadCrumbs[breadCrumbs.length - 2].url} passHref>
+            <IconButton size="small" color="secondary">
+              <ArrowBackIcon />
+            </IconButton>
+          </Link>
         )}
         {breadCrumbs?.map((item, index, arr) => {
           return (
