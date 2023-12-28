75094362c4aa222d826f32fab1ddf106b9d5e8ab
mbaluev <mikhail.baluev@gmail.com>
2023-12-28 01:32:46 +0100

    core

--- src/ui/pages/admin/settings/user/item/quick.tsx
+++ src/ui/pages/admin/settings/user/item/quick.tsx
@@ -2,8 +2,16 @@ import { IconButton, Stack } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
+import { useRouter } from 'next/router';
+import { ROUTES } from '@settings/routes';
 
 export const Quick = () => {
+  const router = useRouter();
+  const handleClose = async () => {
+    await router.push({
+      pathname: ROUTES.ADMIN_SETTINGS_USERS.path,
+    });
+  };
   return (
     <Stack direction="row" spacing={2}>
       <IconButton color="secondary" disabled>
@@ -12,7 +20,7 @@ export const Quick = () => {
       <IconButton color="primary">
         <DeleteIcon />
       </IconButton>
-      <IconButton color="primary">
+      <IconButton color="primary" onClick={handleClose}>
         <CloseIcon />
       </IconButton>
     </Stack>
