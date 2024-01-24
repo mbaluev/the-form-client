b1d81a303c178e9853276c8eb91755ed5c12cf6b
mbaluev <mikhail.baluev@gmail.com>
2024-01-24 01:28:14 +0100

    v2: admin/settings/moduleBlock

--- src/ui/pages/admin/settings/block/item/separator.tsx
+++ src/ui/pages/admin/settings/block/item/separator.tsx
@@ -2,9 +2,19 @@ import { observer } from 'mobx-react';
 import { SeparatorBase } from '@ui/layout/card/separator';
 import { ProgressBase } from '@ui/layout/card/progress';
 import { useBlockItemStore } from '@store/modules/entities/block/item/useBlockItemStore';
+import { useRouter } from 'next/router';
+import { ROUTES } from '@settings/routes';
 
 export const Separator = observer(() => {
   const { isSaveLoading } = useBlockItemStore();
+
+  const router = useRouter();
+  const isCreate =
+    router.pathname === ROUTES.ADMIN_SETTINGS_BLOCK_CREATE.path ||
+    router.pathname === ROUTES.ADMIN_SETTINGS_MODULE_BLOCK_CREATE.path;
+
+  if (!isCreate) return null;
   if (isSaveLoading) return <ProgressBase />;
+
   return <SeparatorBase />;
 });
--- src/ui/pages/admin/settings/module/item/separator.tsx
+++ src/ui/pages/admin/settings/module/item/separator.tsx
@@ -2,9 +2,17 @@ import { observer } from 'mobx-react';
 import { SeparatorBase } from '@ui/layout/card/separator';
 import { useModuleItemStore } from '@store/modules/entities/module/item/useModuleItemStore';
 import { ProgressBase } from '@ui/layout/card/progress';
+import { ROUTES } from '@settings/routes';
+import { useRouter } from 'next/router';
 
 export const Separator = observer(() => {
   const { isSaveLoading } = useModuleItemStore();
+
+  const router = useRouter();
+  const isCreate = router.pathname === ROUTES.ADMIN_SETTINGS_MODULE_CREATE.path;
+
+  if (!isCreate) return null;
   if (isSaveLoading) return <ProgressBase />;
+
   return <SeparatorBase />;
 });
