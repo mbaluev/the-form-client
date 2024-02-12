35213cee5f552de2aaa999092703a4a8603305a2
mbaluev <mikhail.baluev@gmail.com>
2024-02-12 14:13:09 +0100

    v2: admin/settings

--- src/pages/admin/settings/block/[...slug].tsx
+++ src/pages/admin/settings/block/[...slug].tsx
@@ -13,6 +13,8 @@ import { DEFAULT_BLOCK } from '@model/entities/block/default';
 import { PageBlock } from '@ui/pages/admin/settings/block/item/page';
 import { PageBlocks } from '@ui/pages/admin/settings/block/index/page';
 import { useModuleListStore } from '@store/modules/entities/module/list/useModuleListStore';
+import { useFilterStore } from '@store/modules/common/filter/useFilterStore';
+import { useModuleItemStore } from '@store/modules/entities/module/item/useModuleItemStore';
 
 const Block = (props: any) => {
   const router = useRouter();
@@ -23,28 +25,51 @@ const Block = (props: any) => {
     data: block,
     isDataLoading: loadingBlock,
   } = useBlockItemStore();
+  const {
+    getData: getModule,
+    setData: setModule,
+    data: module,
+    isDataLoading: loadingModule,
+  } = useModuleItemStore();
   const { getData: getModules, setData: setModules } = useModuleListStore();
+  const { filters } = useFilterStore();
 
   const breadCrumbs: TBreadCrumb[] = [
     {
       label: ROUTES.HOME.label,
       url: { pathname: ROUTES.HOME.path },
     },
-    {
+  ];
+  if (module) {
+    breadCrumbs.push({
+      label: loadingModule ? (
+        <Skeleton width={100} />
+      ) : module ? (
+        module.title
+      ) : (
+        ROUTES.ADMIN_SETTINGS_MODULE.label
+      ),
+      url: {
+        pathname: ROUTES.ADMIN_SETTINGS_MODULE.path,
+        query: { slug: [filters.moduleId, ROUTES.ADMIN_SETTINGS_MODULE.tabs.keys.blocks] },
+      },
+    });
+  } else {
+    breadCrumbs.push({
       label: ROUTES.ADMIN_SETTINGS_BLOCKS.label,
       url: { pathname: ROUTES.ADMIN_SETTINGS_BLOCKS.path },
-    },
-    {
-      label: loadingBlock ? (
-        <Skeleton width={100} />
-      ) : block ? (
-        block.title
-      ) : (
-        ROUTES.ADMIN_SETTINGS_BLOCK.label
-      ),
-      url: { pathname: ROUTES.ADMIN_SETTINGS_BLOCK.path, query: router.query },
-    },
-  ];
+    });
+  }
+  breadCrumbs.push({
+    label: loadingBlock ? (
+      <Skeleton width={100} />
+    ) : block ? (
+      block.title
+    ) : (
+      ROUTES.ADMIN_SETTINGS_BLOCK.label
+    ),
+    url: { pathname: ROUTES.ADMIN_SETTINGS_BLOCK.path, query: router.query },
+  });
 
   useEffect(() => {
     getModules();
@@ -55,6 +80,11 @@ const Block = (props: any) => {
     if (id) getBlock(id);
     return () => setBlock();
   }, [id]);
+
+  useEffect(() => {
+    if (filters.moduleId) getModule(filters.moduleId);
+    return () => setModule();
+  }, [filters.moduleId]);
 
   const methods = useForm<IBlockDTO>({ mode: 'all', defaultValues: DEFAULT_BLOCK });
   useEffect(() => {
